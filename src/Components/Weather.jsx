83c3c90b8c139caadfbdcba1f204bfc9cef6573e
Binada-SH <90200965+Binada-SH@users.noreply.github.com>
2025-03-31 15:10:07 +0530

    Adding additional weather information

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -17,8 +17,25 @@ const Weather = () => {
           <input type="text" placeholder = 'Search'/>
           <img src={search_icon} alt="" />
         </div>
-        <img src={clear_icon} alt="" />
-      
+        <img src={clear_icon} alt="" className = 'weather_icon'/>
+        <p className = 'temperature'>24 °C</p>
+        <p className = 'city_name'>BERLIN</p>
+        <div className="weather_data">
+          <div className="col">
+            <img src={humidity_icon} alt="" />
+            <div className="humid_text">
+              <p>80% </p>
+              <span>Humidity</span>
+            </div>
+          </div>
+          <div className="col">
+            <img src={wind_icon} alt="" />
+            <div className="wind_text">
+              <p>2.8 Km/h</p>
+              <span>Wind Speed</span>
+            </div>
+          </div>
+        </div>
       
     </div>
   )
