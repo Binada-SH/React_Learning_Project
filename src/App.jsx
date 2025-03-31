8ae555731967c182c01558f43c200bea32ab69df
Binada-SH <90200965+Binada-SH@users.noreply.github.com>
2025-03-31 19:45:10 +0530

    change files

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,7 @@ import React from 'react'
 import Weather from './Components/Weather'
 
 const App = () => {
-  const search = async (city) => {
-    try {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`
-      const response = await fetch(url);
-      const data = await response.json();
-      console.log(data);
-      
-      
-    } catch (error) {
-      
-    }
-  }
+  
   return (
     <div className = "app">
       <Weather/>
