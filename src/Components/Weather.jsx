381e6d8d9831c2f986a268434d2756dddacff4e6
Binada-SH <90200965+Binada-SH@users.noreply.github.com>
2025-03-31 13:27:46 +0530

    Importing assests and designing the search bar ui

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './Weather.css'
+import search_icon from'../assets/search.png'
 const Weather = () => {
   return (
     <div className = 'weather'>
       <h1>Weather</h1>
+        <div className="search_bar">
+          <input type="text" placeholder = 'Search'/>
+          <img src={search_icon} alt="" />
+        </div>
+      
       
     </div>
   )
